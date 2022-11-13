6b373aa5eb0c0d730fb7ac8b2e2cdad61f9622fb
Kritika Srivastava <kritikasri27@gmail.com>
2022-11-13 12:34:55 +0530

    Added image and download button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import Title from "./Title"
+import Qr from "./Qr";
 
 function App() {
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      
+        <img style={{width:"50vw",height:"100vh"}}src="./brooke-lark-RrzeCGujVfU-unsplash.jpg" alt="food"/>
+      
+      <div className="scan">
+      <Title/>
+      <Qr/>
+      </div>
     </div>
   );
 }
