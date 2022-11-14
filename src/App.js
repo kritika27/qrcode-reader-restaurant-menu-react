fefc34f4c022f638c72559cb0344f2a5d73ab651
Kritika Srivastava <kritikasri27@gmail.com>
2022-11-14 21:14:01 +0530

    Added toggle button to display text in English or German

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from "react"
+import React,{useState} from "react"
 import './App.css';
 import Title from "./Title"
 import Qr from "./Qr";
 
 function App() {
+  const [toggle,setToggle]=useState(false);
     
   return (
     <div className="app">
       
-        <img style={{width:"50vw",height:"100vh"}}src="./brooke-lark-RrzeCGujVfU-unsplash.jpg" alt="food"/>
+        <img style={{width:"50vw",height:"100vh"}}src="./brooke-lark-RrzeCGujVfU-unsplash.jpg" 
+        alt="varieties of food on the plates on the table"/>
       
       <div className="scan">
-      <Title/>
-      <Qr/>
+      <nav>
+        <ul>
+          {!toggle && <li><button className="toggle" onClick={()=>setToggle(true)} >DE</button></li>}
+      {toggle && <li><button className="disabled"  disabled>DE</button></li>}
+      {toggle && <li><button className="toggle" onClick={()=>setToggle(false)} >EN</button></li>}
+      {!toggle && <li><button className="disabled"  disabled>EN</button></li>}
+      
+        </ul>
+      </nav>
+        
+      <Title toggle={toggle}/>
+      <Qr toggle={toggle}/>
+      
       </div>
     </div>
   );
